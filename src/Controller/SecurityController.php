b65c921c7c7ae13043222b197bf54750e7bdeef2
user-penguin <kobzev96@gmail.com>
2019-08-16 05:35:59 +0700

    fix permission and add dashboard controller

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -25,4 +25,11 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername
         ]);
     }
+
+     /**
+      * @Route("/logout", name="logout")
+      */
+    public function logout() {
+
+    }
 }
