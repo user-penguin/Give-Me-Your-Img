d43ef3a0d28cb5e07f40a2f453ccce8ee25d2501
user-penguin <kobzev96@gmail.com>
2019-08-16 07:11:15 +0700

    fixed auth settings

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,11 +3,13 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
  */
 class User implements UserInterface, \Serializable
 {
